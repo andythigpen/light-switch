47a80095542a27a03aff05c197df05e157cf0e77
Andrew Thigpen <andyt05@gmail.com>
2015-01-12 22:01:18 -0600

    Add basic radio communication code for hub.
    Testing to make sure the hub and switch can actually communicate
    with each other using the RFM12b library.

--- hub/src/firmware.ino
+++ hub/src/firmware.ino
@@ -1,11 +1,54 @@
 //
 // Capacitive touch light switch hub
 //
+#include <RFM12B.h>
+
+RFM12B radio;
+
+#define NODEID      1
+#define NETWORKID   1
+#define FREQUENCY   RF12_915MHZ
+#define ACK_TIME    30  // # of ms to wait for an ack
+
+typedef struct {
+    int nodeid;
+    int event;
+    int electrode;
+} Payload;
+Payload data;
 
 void setup() {
     Serial.begin(57600);
     while (!Serial);
+
+    radio.Initialize(NODEID, FREQUENCY, NETWORKID);
 }
 
 void loop() {
+    if (radio.ReceiveComplete()) {
+        if (radio.CRCPass()) {
+            Serial.print('[');
+            Serial.print(radio.GetSender());
+            Serial.print(']');
+
+            if (*radio.DataLen != sizeof(Payload)) {
+                Serial.println("bad payload");
+            }
+            else {
+                data = *(Payload *)radio.Data;
+                Serial.print("node:  ");
+                Serial.print(data.nodeid);
+                Serial.print(" event: ");
+                Serial.print(data.event);
+                Serial.print(" elec:  ");
+                Serial.print(data.electrode);
+            }
+
+            if (radio.ACKRequested()) {
+                byte nodeid = radio.GetSender();
+                radio.SendACK();
+            }
+            Serial.println();
+        }
+    }
 }
