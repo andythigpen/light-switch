9b0c28165c909aa0ebd951f9cc190b61c1786ddd
andythigpen <andyt05@gmail.com>
2015-02-07 14:00:13 -0600

    Adds command shell.
    Adds dump settings, reset, and set byte commands to hub/shell.

--- hub/src/firmware.ino
+++ hub/src/firmware.ino
@@ -14,17 +14,27 @@ RFM12B radio;
 #define ACK_TIME    30  // # of ms to wait for an ack
 
 #define CMD_MSG             0
-#define CMD_TOUCH_EVENT     1
-#define CMD_STATUS_EVENT    2
-#define CMD_DUMP_SETTINGS   3
+#define CMD_ACK             1
+#define CMD_TOUCH_EVENT     2
+#define CMD_STATUS_EVENT    3
+#define CMD_DUMP_SETTINGS   4
+#define CMD_RESET           5
+#define CMD_SET_BYTE        6
 
 CmdMessenger cmd(Serial);
+struct {
+    byte nodeId;
+    byte pkt[256];
+} command;
 
 void setup() {
     Serial.begin(115200);
     radio.Initialize(NODEID, FREQUENCY, NETWORKID);
 
     cmd.attach(onUnknownCommand);
+    cmd.attach(CMD_RESET, onResetCommand);
+    cmd.attach(CMD_DUMP_SETTINGS, onDumpCommand);
+    cmd.attach(CMD_SET_BYTE, onSetByteCommand);
     cmd.sendCmd(CMD_MSG, "Initialized...");
 }
 
@@ -32,40 +42,79 @@ void onUnknownCommand() {
     cmd.sendCmd(CMD_MSG, "Unknown command");
 }
 
-void handleTouchEvent() {
+void onResetCommand() {
+    command.nodeId = (byte)cmd.readCharArg();
+    SwitchReset *rst = (SwitchReset *)&command.pkt;
+    rst->type = SwitchPacket::RESET;
+    rst->len = sizeof(SwitchReset);
+    rst->resetSettings = cmd.readBoolArg() ? 1 : 0;
+    cmd.sendCmd(CMD_ACK, "ok");
+}
+
+void onDumpCommand() {
+    command.nodeId = (byte)cmd.readCharArg();
+    SwitchDumpSettings *pkt = (SwitchDumpSettings *)&command.pkt;
+    pkt->type = SwitchPacket::DUMP_REQUEST;
+    pkt->len = sizeof(SwitchPacket);
+    cmd.sendCmd(CMD_ACK, "ok");
+}
+
+void onSetByteCommand() {
+    byte nodeId = (byte)cmd.readCharArg();
+    byte offset = (byte)cmd.readCharArg();
+    byte value = (byte)cmd.readCharArg();
+    SwitchConfigure *pkt = (SwitchConfigure *)&command.pkt;
+    if (nodeId == command.nodeId && pkt->type == SwitchPacket::CONFIGURE) {
+        byte *p = (byte *)pkt;
+        p[pkt->len++] = offset;
+        p[pkt->len++] = value;
+    }
+    else {
+        command.nodeId = nodeId;
+        pkt->type = SwitchPacket::CONFIGURE;
+        pkt->len = sizeof(SwitchConfigure) + sizeof(SwitchConfigureByte);
+        pkt->cfg[0] = offset;
+        pkt->cfg[1] = value;
+    }
+    cmd.sendCmdStart(CMD_ACK);
+    cmd.sendCmdfArg("%c %c %c", nodeId, offset, value);
+    cmd.sendCmdEnd();
+}
+
+void handleTouchEvent(byte nodeId) {
     if (*radio.DataLen != sizeof(TouchEvent)) {
         cmd.sendCmd(CMD_MSG, "bad touch event payload");
         return;
     }
     TouchEvent pkt = *(TouchEvent *)radio.Data;
     cmd.sendCmdStart(CMD_TOUCH_EVENT);
-    cmd.sendCmdArg(radio.GetSender());
+    cmd.sendCmdArg(nodeId);
     cmd.sendCmdArg(pkt.gesture);
     cmd.sendCmdArg(pkt.electrode);
     cmd.sendCmdArg(pkt.repeat);
     cmd.sendCmdEnd();
 }
 
-void handleStatusUpdate() {
+void handleStatusUpdate(byte nodeId) {
     if (*radio.DataLen != sizeof(SwitchStatus)) {
         cmd.sendCmd(CMD_MSG, "bad status event payload");
         return;
     }
     SwitchStatus pkt = *(SwitchStatus *)radio.Data;
     cmd.sendCmdStart(CMD_STATUS_EVENT);
-    cmd.sendCmdArg(radio.GetSender());
+    cmd.sendCmdArg(nodeId);
     cmd.sendCmdArg(pkt.batteryLevel);
     cmd.sendCmdEnd();
 }
 
-void handleSettingsDump() {
+void handleSettingsDump(byte nodeId) {
     if (*radio.DataLen != sizeof(SwitchDumpSettings)) {
         cmd.sendCmd(CMD_MSG, "bad settings dump payload");
         return;
     }
     SwitchDumpSettings pkt = *(SwitchDumpSettings *)radio.Data;
     cmd.sendCmdStart(CMD_DUMP_SETTINGS);
-    cmd.sendCmdArg(radio.GetSender());
+    cmd.sendCmdArg(nodeId);
     cmd.sendCmdBinArg(pkt.settings);
     cmd.sendCmdEnd();
 }
@@ -75,16 +124,17 @@ void handleIncomingPacket() {
         return;
     }
 
+    byte nodeId = radio.GetSender();
     unsigned char type = *(unsigned char *)radio.Data;
     switch (type) {
         case SwitchPacket::TOUCH_EVENT:
-            handleTouchEvent();
+            handleTouchEvent(nodeId);
             break;
         case SwitchPacket::STATUS_UPDATE:
-            handleStatusUpdate();
+            handleStatusUpdate(nodeId);
             break;
         case SwitchPacket::DUMP_REPLY:
-            handleSettingsDump();
+            handleSettingsDump(nodeId);
             break;
         default:
             cmd.sendCmd(CMD_MSG, "unknown event");
@@ -92,9 +142,14 @@ void handleIncomingPacket() {
     }
 
     if (radio.ACKRequested()) {
-        //TODO: send reconfiguration packet, if available
-        SwitchPacket ping(SwitchPacket::PING, sizeof(SwitchPacket));
-        radio.SendACK((void *)&ping, sizeof(ping));
+        if (command.nodeId == nodeId) {
+            cmd.sendCmd(0, "Sending command packet");
+            SwitchPacket *hdr = (SwitchPacket *)&command.pkt;
+            radio.SendACK((void *)&command.pkt, hdr->len);
+            command.nodeId = 0;
+        }
+        else
+            radio.SendACK();
     }
 }
 
--- pc/controller.py
+++ pc/controller.py
@@ -1,38 +1,181 @@
 import serial
 from cmdmessenger import CmdMessenger
+import threading
 import binascii
+import cmd
 
-s = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=1)
-cmd = CmdMessenger(s)
+class Command:
+    MSG             = 0
+    ACK             = 1
+    TOUCH_EVENT     = 2
+    STATUS_EVENT    = 3
+    DUMP_SETTINGS   = 4
+    RESET           = 5
+    SET_BYTE        = 6
 
-#TODO: add command line options to send reconfigure packet
+ack_condition = threading.Condition()
+ack_msg = None
 
-@CmdMessenger.callback(cmdid=0)
-def handle_debug(cmd):
-    print(cmd.read_str())
+@CmdMessenger.callback(cmdid=Command.MSG)
+def handle_debug(msg):
+    print("debug: {}".format(msg.read_str()))
 
-@CmdMessenger.callback(cmdid=1)
-def handle_touch_event(cmd):
-    nodeid = cmd.read_int8()
-    gesture = cmd.read_int8()
-    electrode = cmd.read_int8()
-    repeat = cmd.read_int8()
+@CmdMessenger.callback(cmdid=Command.ACK)
+def handle_ack(msg):
+    global ack_msg
+    ack_condition.acquire()
+    ack_msg = msg
+    ack_condition.notify()
+    ack_condition.release()
+
+@CmdMessenger.callback(cmdid=Command.TOUCH_EVENT)
+def handle_touch_event(msg):
+    nodeid = msg.read_int8()
+    gesture = msg.read_int8()
+    electrode = msg.read_int8()
+    repeat = msg.read_int8()
     print('[{}] gesture: {}, electrode: {} repeat: {}'.format(nodeid,
         gesture, electrode, repeat))
 
-@CmdMessenger.callback(cmdid=2)
-def handle_status_event(cmd):
-    nodeid = cmd.read_int8()
-    vcc = cmd.read_int8()
+@CmdMessenger.callback(cmdid=Command.STATUS_EVENT)
+def handle_status_event(msg):
+    nodeid = msg.read_int8()
+    vcc = msg.read_int8()
     print("[{}] status: vcc {}".format(nodeid, vcc))
 
-@CmdMessenger.callback(cmdid=3)
-def handle_dump_settings(cmd):
-    nodeid = cmd.read_int8()
+@CmdMessenger.callback(cmdid=Command.DUMP_SETTINGS)
+def handle_dump_settings(msg):
+    nodeid = msg.read_int8()
     print("[{}] Dump settings:".format(nodeid))
-    settings = cmd.read_bytes()
+    settings = msg.read_bytes()
     settings = str(binascii.hexlify(settings), 'ascii')
     print(':'.join(settings[i:i+2] for i in range(0, len(settings), 2)))
 
-while True:
-    cmd.read()
+
+class SerialInputThread(threading.Thread):
+    def __init__(self, msg):
+        super(SerialInputThread, self).__init__()
+        self.msg = msg
+        self.running = False
+
+    def run(self):
+        self.running = True
+        while self.running:
+            self.msg.read()
+
+    def wait_for_ack(self, timeout=None):
+        global ack_msg
+        ack_condition.acquire()
+        ack_msg = None
+        ack_condition.wait(timeout)
+        ret = ack_msg
+        ack_condition.release()
+        return ack_msg
+
+
+class ControllerShell(cmd.Cmd):
+    intro = 'switch controller shell.  Type help or ? to list commands.\n'
+    prompt = 'command> '
+    connected = False
+
+    def emptyline(self):
+        return
+
+    def do_connect(self, args):
+        'Connect to a serial port'
+        if self.connected:
+            return
+        port, args = args.partition(' ')[::2]
+        baud, args = args.partition(' ')[::2]
+        timeout, args = args.partition(' ')[::2]
+        port = port or '/dev/ttyUSB0'
+        baud = int(baud or 115200)
+        timeout = float(timeout or 1.0)
+        print("Connecting to {} at {} baud, timeout {}".format(port, baud, timeout))
+        try:
+            self.connection = serial.Serial(port, baudrate=baud, timeout=timeout)
+        except serial.serialutil.SerialException as e:
+            print(e)
+            return
+        self.msg = CmdMessenger(self.connection)
+        print("Starting input thread...")
+        self.input_thread = SerialInputThread(self.msg)
+        self.input_thread.start()
+        self.connected = True
+
+    def _reset_command(self, nodeid, hard=False):
+        with self.msg.writer(cmdid=Command.RESET) as w:
+            w.send_char(int(nodeid))
+            w.send_bool(hard)
+        msg = self.input_thread.wait_for_ack(1.0)
+        if msg:
+            print('Tap switch {} to reset.'.format(nodeid))
+        else:
+            print('No ACK received')
+
+    def do_reset(self, args):
+        'Reset a switch given a node id: reset 2'
+        nodeid, args = args.partition(' ')[::2]
+        if not nodeid:
+            print('Missing required nodeid')
+            return
+        self._reset_command(nodeid)
+
+    def do_hardreset(self, args):
+        'Reset a switch and its settings given a node id: hardreset 2'
+        nodeid, args = args.partition(' ')[::2]
+        if not nodeid:
+            print('Missing required nodeid')
+            return
+        self._reset_command(nodeid, hard=True)
+
+    def do_dump(self, args):
+        'Dump switch settings given a node id: dump 2'
+        nodeid, args = args.partition(' ')[::2]
+        if not nodeid:
+            print('Missing required nodeid')
+            return
+        with self.msg.writer(cmdid=Command.DUMP_SETTINGS) as w:
+            w.send_char(int(nodeid))
+        msg = self.input_thread.wait_for_ack(1.0)
+        if msg:
+            print('Tap switch {} to dump settings.'.format(nodeid))
+        else:
+            print('No ACK received')
+
+    def do_setbyte(self, args):
+        'Sets a configuration byte, given nodeid, offset, value: setbyte 2 2 2'
+        nodeid, args = args.partition(' ')[::2]
+        offset, args = args.partition(' ')[::2]
+        value, args = args.partition(' ')[::2]
+        if not nodeid or not offset or not value:
+            print('Missing required argument')
+            return
+        print('{} {} {}'.format(int(nodeid), int(offset, 0), int(value, 0)))
+        with self.msg.writer(cmdid=Command.SET_BYTE) as w:
+            w.send_char(int(nodeid))
+            w.send_char(int(offset, 0))
+            w.send_char(int(value, 0))
+        msg = self.input_thread.wait_for_ack(1.0)
+        if msg:
+            print('Tap switch {} to set configuration.'.format(nodeid))
+            print(msg.read_str())
+        else:
+            print('No ACK received')
+
+    def do_exit(self, args):
+        'Exits the shell'
+        if self.connected:
+            self.connection.close()
+            self.input_thread.running = False
+        return True
+
+if __name__ == '__main__':
+    c = ControllerShell()
+    try:
+        c.cmdloop()
+    except KeyboardInterrupt:
+        print()
+    finally:
+        if c.connected:
+            c.input_thread.running = False
